8becb604b3a6a0c00caab696406d9e3793091ab5
Rajat Sharma <rajat.sharma@calsofthq.com>
2019-12-26 12:08:49 +0530

    addition of new files

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"task-missing-param/utils"
 )
 
 func main() {
-    fmt.Println("hello world")
+	fmt.Println("hello world")
+	BootDetails := utils.ExtractBootDetails()
+	CPUDetails := utils.ExtractCPUDetails()
+	fmt.Printf("\nBoot Details-:\nCurrentBootMode: %s\nPxeInterface: %s\n", BootDetails.CurrentBootMode, BootDetails.PxeInterface)
+	fmt.Printf("\nCPU Details-:\ncount: %d\nfrequency: %s\nflags:  %s\nmodel_name: %s\narchitecture: %s\n", CPUDetails.Count, CPUDetails.Frequency, CPUDetails.Flags, CPUDetails.ModelName, CPUDetails.Architecture)
+	fmt.Println(CPUDetails.NumaTopology)
 }
